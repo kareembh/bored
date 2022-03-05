61c111583dfec23910ea50928cd4193b99056be0
Kareem Brathwaite-Henry <kareembrathwaite-henry@Kareems-MacBook-Pro.local>
2022-03-05 12:24:51 -0500

    changed button code to input button type

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,7 @@
 import './Button.css'
 const Button = (props) =>{
     return(
-        <div className="btn" onClick={props.handleClick}>
-            <p className="btn-text">{props.buttonText}</p>
-        </div>
+        <input className="btn" type="button" value={props.buttonText} onClick={props.handleClick} />
     )
 }
 
