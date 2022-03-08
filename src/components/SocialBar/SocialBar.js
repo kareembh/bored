c0188a421deab2ea1fc4006c8b933dd469ea68f9
Kareem Brathwaite-Henry <kareembrathwaite-henry@Kareems-MacBook-Pro.local>
2022-03-08 10:58:33 -0500

    added class names to socalbar

--- src/components/SocialBar/SocialBar.js
+++ src/components/SocialBar/SocialBar.js
@@ -4,13 +4,13 @@ const SocialBar = () =>{
         <div className="social-icons top-right">
             <ul>
                 <a href="https://www.instagram.com/kareemave/">
-                    <img className="social-icon" src="../../assets/icons/icons8-instagram.svg" alt="" />
+                    <img className="social-icon" src='./../../assets/icons/icons8-instagram.svg' alt="" />
                 </a>
                 <a href="https://medium.com/@kareembhenry">
-                    <img className="social-icon" src="../../assets/icons/icons8-medium.svg"  alt="" />
+                    <img className="social-icon" src="./../../assets/icons/icons8-medium.svg"  alt="" />
                 </a>
                 <a href="https://www.youtube.com/channel/UCQqRwZ85wC3cXxW4UeZvYhQ">
-                    <img className="social-icon" src="../../assets/icons/icons8-youtube.svg" alt="" />
+                    <img className="social-icon" src="./../../assets/icons/icons8-youtube.svg" alt="" />
                 </a>
             </ul>
         </div>
