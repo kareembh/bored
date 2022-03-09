fdb315803bed914b95bf567666ebcd60392e5fd9
Kareem Brathwaite-Henry <kareembrathwaite-henry@Kareems-MacBook-Pro.local>
2022-03-09 08:31:58 -0500

    added SVG social icons and additional styling

--- src/components/SocialBar/SocialBar.js
+++ src/components/SocialBar/SocialBar.js
@@ -1,16 +1,23 @@
 import './SocialBar.css'
+import InstagramIcon from './../../assets/icons/instagram.svg'
+import LinkedinIcon from './../../assets/icons/linkedin.svg'
+import MediumIcon from './../../assets/icons/medium.svg'
+import GithubIcon from './../../assets/icons/github.svg'
 const SocialBar = () =>{
     return(
         <div className="social-icons top-right">
             <ul>
                 <a href="https://www.instagram.com/kareemave/">
-                    <img className="social-icon" src='./../../assets/icons/icons8-instagram.svg' alt="" />
+                    <img className="social-icon" src={InstagramIcon} alt='instagram icon' />
+                </a>
+                <a href="https://github.com/kareembh">
+                    <img className="social-icon" src={GithubIcon} alt='linkedin icon' />
                 </a>
                 <a href="https://medium.com/@kareembhenry">
-                    <img className="social-icon" src="./../../assets/icons/icons8-medium.svg"  alt="" />
+                    <img className="social-icon" src={MediumIcon}  alt='medium icon' />
                 </a>
-                <a href="https://www.youtube.com/channel/UCQqRwZ85wC3cXxW4UeZvYhQ">
-                    <img className="social-icon" src="./../../assets/icons/icons8-youtube.svg" alt="" />
+                <a href="https://www.linkedin.com/in/kareem-brathwaite-henry-399262a1/">
+                    <img className="social-icon" src={LinkedinIcon} alt='linkedin icon' />
                 </a>
             </ul>
         </div>
